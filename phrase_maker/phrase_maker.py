ed0c3869207adf8fcc3e8c514719dfd8f0ec8cb0
Russell White <rarctem@gmail.com>
2020-11-25 12:57:57 -0800

    Fix wrong comparison operator.

--- phrase_maker/phrase_maker.py
+++ phrase_maker/phrase_maker.py
@@ -5,27 +5,27 @@ import re
 import sys
 
 data = {
-    #Some general use phrases.
-    'gen' : {
-        'template' : ['this is only so tests don\'t crash'],
+    # Some general use phrases.
+    'gen': {
+        'template': ['this is only so tests don\'t crash'],
 
-        'username' : ['{.}[username]'],
+        'username': ['{.}[username]'],
 
-        'vowel' : list('aeiouy'),
-        'consonant' : list('bcdfghjklmnpqrstvwxyz'),
-        'karst' : ['Karst', 'Flannery', 'Flanneroo'],
+        'vowel': list('aeiouy'),
+        'consonant': list('bcdfghjklmnpqrstvwxyz'),
+        'karst': ['Karst', 'Flannery', 'Flanneroo'],
 
-        'num_either' : ['{number}', '{roman_number}'],
+        'num_either': ['{number}', '{roman_number}'],
 
-        'number' : ['{digit}{number}', '{digit}'],
-        'digit' : '1234567890',
+        'number': ['{digit}{number}', '{digit}'],
+        'digit': '1234567890',
 
-        'roman_number' : ['{roman_ones}']*3 + ['{roman_tens}{roman_ones}'],
-        'roman_tens' : ['X'],
-        'roman_ones' : ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+        'roman_number': ['{roman_ones}'] * 3 + ['{roman_tens}{roman_ones}'],
+        'roman_tens': ['X'],
+        'roman_ones': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
 
-        'weekday' : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-            'Friday', 'Saturday'],
+        'weekday': ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+                    'Friday', 'Saturday'],
     }
 }
 
@@ -38,6 +38,7 @@ _search_string = '{' + _books_match + '}' + '(?:' + _name_match + ')?'
 
 TAG_REGEX = re.compile(_search_string)
 
+
 def load_module(module):
     new_data = module.data
     for i in new_data.keys():
@@ -47,8 +48,8 @@ def load_module(module):
             data[i] = new_data[i]
 
 
-#Constructs a full random string using the named library.
-def make(lib_name, name = 'Dudeface', capitalize = True):
+# Constructs a full random string using the named library.
+def make(lib_name, name='Dudeface', capitalize=True):
     orig = random.choice(data[lib_name]['template'])
     fixed_data = {'username': name}
 
@@ -66,14 +67,15 @@ def make(lib_name, name = 'Dudeface', capitalize = True):
     orig = fix_punctuation(orig)
     return orig
 
+
 def replace_tags(lib_name, orig, fixed_data):
     while TAG_REGEX.search(orig) is not None:
         to_replace = TAG_REGEX.search(orig)
         books = to_replace.group('books')
         name = to_replace.group('name')
 
-        #if we have multiple comma-separated fields, choose one
-        #TODO: add weights based on number of elements
+        # if we have multiple comma-separated fields, choose one
+        # TODO: add weights based on number of elements
         book = random.choice(books.split(','))
 
         capitalize = book[0] != book[0].lower()
@@ -89,7 +91,7 @@ def replace_tags(lib_name, orig, fixed_data):
         else:
             word = random.choice(data[library][book])
 
-        #Handle all the extra vars inside the new word before we store it in fixed_data.
+        # Handle all the extra vars inside the new word before we store it in fixed_data.
         word = replace_tags(library, word, fixed_data)
 
         if name and name not in fixed_data:
@@ -100,11 +102,12 @@ def replace_tags(lib_name, orig, fixed_data):
         orig = TAG_REGEX.sub(word, orig, 1)
     return orig
 
+
 def make_capital(orig):
     for char in ' \n':
         orig = orig.split(char)
         for i in range(len(orig)):
-            if i is not 0 and orig[i] in ['of', 'the', 'a']:
+            if i != 0 and orig[i] in ['of', 'the', 'a']:
                 continue
             if orig[i][0] == '{':
                 orig[i] = orig[i][0] + orig[i][1].capitalize() + orig[i][2:]
@@ -113,7 +116,9 @@ def make_capital(orig):
         orig = char.join(orig)
     return orig
 
-#Makes sure sentences start with capitals.
+# Makes sure sentences start with capitals.
+
+
 def fix_capitals(orig):
     for char in ' \n':
         orig = orig.split(char)
@@ -122,6 +127,7 @@ def fix_capitals(orig):
                 orig[i] = orig[i][0].upper() + orig[i][1:]
         orig = char.join(orig)
     return orig
+
 
 def fix_articles(orig):
     orig = orig.split(' ')
@@ -133,38 +139,41 @@ def fix_articles(orig):
                 orig[i] = 'a'
     return ' '.join(orig)
 
+
 def fix_punctuation(orig):
     #find_punc = re.compile('''[.!?][.!?]+''')
 
-    #reduce sequences of . to 3 max
+    # reduce sequences of . to 3 max
     while orig.count('....'):
         orig = orig.replace('....', '...')
 
-    #now temporarily convert '...' into a temp unicode char (…) to preserve it
+    # now temporarily convert '...' into a temp unicode char (…) to preserve it
     orig = orig.replace('...', u'…')
 
-    #get rid of ..
+    # get rid of ..
     orig = orig.replace('..', '.')
 
-    #let ! override .
+    # let ! override .
     orig = orig.replace('!.', '!').replace('.!', '!')
 
-    #let ? override .
+    # let ? override .
     orig = orig.replace('?.', '?').replace('.?', '?')
 
-    #standardize !? and ?!
+    # standardize !? and ?!
     orig = orig.replace('!?', '!?').replace('?!', '!?')
 
-    #undo the previous substitution
+    # undo the previous substitution
 
     orig = orig.replace(u'…', '...')
     return orig
+
 
 def get_categories(dict_name):
     if dict_name in data.keys():
         return '{}'.format(sorted(data[dict_name].keys()))
     else:
         return 'No database by that name.'
+
 
 def main():
     if 'test' in sys.argv:
@@ -177,11 +186,13 @@ def main():
     else:
         print(make('movie'))
 
+
 def test():
     for key in sorted(data.keys()):
         print('Testing {}'.format(key))
         for i in range(10000):
             make(key)
+
 
 def demo():
     for key in sorted(data.keys()):
